910297c30bc44f404b9a7c2ec688101178ddacbc
AhmedRaza186 <sallumoon650@gmail.com>
2025-10-24 17:38:46 +0500

    hero

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 function changeTheme() {
-      document.body.classList.toggle("dark-mode");
-      document.body.classList.toggle("light-mode");
+  document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("light-mode");
   let toggle = document.querySelector('.toggle i');
   toggle.classList.toggle('fa-toggle-on');
   toggle.classList.toggle('fa-toggle-off');
 }
+
 function openSidebar() {
   const sidebar = document.querySelector('.sidebar')
   sidebar.style.transform = 'translateX(0%)'
   document.querySelector('.car-toggle').style.transform = 'translateX(-60vw)'
-  document.querySelector('.car-toggle').setAttribute('onclick','closeSidebar()')
+  document.querySelector('.car-toggle').setAttribute('onclick', 'closeSidebar()')
 }
+
 function closeSidebar() {
   const sidebar = document.querySelector('.sidebar')
   sidebar.style.transform = 'translateX(100%)'
   document.querySelector('.car-toggle').style.transform = 'translateX(0vw)'
-  document.querySelector('.car-toggle').setAttribute('onclick','openSidebar()')
+  document.querySelector('.car-toggle').setAttribute('onclick', 'openSidebar()')
 }
-function lightsOn(){
-  document.querySelector('.car img').src = "./assets/rollsroyce/roycelightson.png"
+
+function doorsOpen() {
+  document.querySelector('.car img').src = "./assets/rollsroyce/roycedoorsopen.png"
 }
-function lightsOff(){
+
+function doorsClose() {
   document.querySelector('.car img').src = "./assets/rollsroyce/roycelightsoff.png"
 }
-// function closeSidebar() {
-//   const sidebar = document.querySelector('.sidebar')
-//   sidebar.style.right = '-100%'
-//   const overlay = document.getElementById('overlay')
-//   setTimeout(() => overlay.style.display = 'none', 200)
-// }
+
+function carEnter(hero) {
+  hero.querySelector('.header').style.opacity = '1'
+  hero.querySelector('.header').style.transition = 'opacity .8s ease-in-out'
+  let desktopCar = hero.querySelector('.desktopcar')
+  let mobileCar = hero.querySelector('.mobilecar')
+  desktopCar.style.animation = 'carEnter 1.2s ease-out forwards'
+  mobileCar.style.animation = 'carEnterMobile 1.2s ease-out forwards'
+  hero.setAttribute('onmouseover', '')
+}
+let carSound = new Audio("./assets/rollsroyce/carstart.mp3")
+function carStart() {
+  document.querySelector('.car img').src = "./assets/rollsroyce/roycelightson.png"
+  carSound.volume = 0.7
+  carSound.play()
+
+}
